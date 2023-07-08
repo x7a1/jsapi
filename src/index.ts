90026e8b07413b967dd835d6ad048db14bbeeada
x7a1 <88008373+x7a1@users.noreply.github.com>
2023-07-08 09:50:32 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ import { app } from './api';
 const port = process.env.PORT || 3333;
 
 app.listen(port, () =>
-  console.log(`API available on http://localhost:${port}`)
+  console.log(`API available on https://express-server-production-0019.up.railway.app/api/v1/hello`)
 );
